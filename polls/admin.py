ab9c88d02c8b285688daf88c514c955dd87c0847
Mohit <msk@ubuntu.(none)>
2013-01-16 17:13:37 +0800

    adding more views

--- polls/admin.py
+++ polls/admin.py
@@ -1,15 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from polls.models import Poll, Choice
 from django.contrib import admin
 
+
 class ChoiceInline(admin.TabularInline):
+
     model = Choice
     extra = 3
 
+
 class PollAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,               {'fields': ['question']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-    ]
+
+    fieldsets = [(None, {'fields': ['question']}), ('Date information',
+                 {'fields': ['pub_date'], 'classes': ['collapse']})]
     list_display = ('question', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
     search_fields = ['question']
@@ -17,5 +21,5 @@ class PollAdmin(admin.ModelAdmin):
 
     inlines = [ChoiceInline]
 
-admin.site.register(Poll, PollAdmin)
 
+admin.site.register(Poll, PollAdmin)
--- polls/views.py
+++ polls/views.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 # Create your views here.
 
 from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render_to_response, get_object_or_404
 from polls.models import Poll
 from django.template import Context, loader
 
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    t = loader.get_template('polls/index.html')
-    c = Context({'latest_poll_list': latest_poll_list})
-    return HttpResponse(t.render(c))
+
+    # Can either do this
+    # t = loader.get_template('index.html')
+    # c = Context({'latest_poll_list': latest_poll_list})
+    # return HttpResponse(t.render(c))
+    # Or use this shortcut below
+
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    return render_to_response('index.html',
+                              {'latest_poll_list': latest_poll_list})
 
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response('detail.html', {'poll': p})
 
 
 def results(request, poll_id):
